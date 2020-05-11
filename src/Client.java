a9a074aff9a4a8907c13a154109ab62fbedc1c46
tonmoy0010 <tonmoyahmed.jitu@gmail.com>
2020-05-11 23:38:51 +1000

    added files from old repo

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,24 @@
-public class Client {a
+/**
+ * This file consists of the client class which is used to initiate the
+ * simulation between the Client-side and the Server-side
+ * @author Tonmoy Ahmed Jitu, 44266278
+ * @author Lakshmi Priya Bhuphatiraju, 45431957
+ * Communication address 127.0.0.1, port 50000
+ **/
+public class Client {
 
+    public static void main(String args[]) {
 
-    asdasdasda
+        Electrode3100 electrode3100 = new Electrode3100("127.0.0.1", 50000);
+
+        if (args.length == 2) {
+            if (args[0].equals("-a")) {
+                if (args[1].equals("ff")) {
+                    electrode3100.algo = "ff";
+                } else if (args[1].equals("wf")) {
+                    electrode3100.algo = "wf";
+            }
+        }
+        electrode3100.run();
+    }
 }
